9903569fc360c7c12e837ecd507a50dee6e75045
ironkiller86 <donatotuzzolino@gmail.com>
2021-04-08 01:09:07 +0200

    continued work with rtk

--- src/Components/CityField/index.js
+++ src/Components/CityField/index.js
@@ -15,7 +15,7 @@ import { fetchWeatherData } from "../../Rtk/Slices/weatherState";
 /*
  *  ant library import
  */
-import { Input, Switch, Popover } from "antd";
+import { Input } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 /*
  * styles import
@@ -27,15 +27,87 @@ import "./styles.css";
 const CityField = memo(({ placeholder }) => {
   console.log("CityField component");
   const [cityField, setCityField] = useState(null);
+  /*const [geoSwitch, setGeoSwitch] = useState(false)*/
   const dispatch = useDispatch();
   const { city, enableAnimation, geolocation, animationEnd } = useSelector(
     (state) => state.weatherDataConfigState /*weatherDataSelector*/
   );
   const host = "https://api.openweathermap.org";
+  /*
+   * 
+   */
+  const handlerSwitch = () => {
+    console.log("CityField component - handlerSwitch");
+    dispatch(enableGeolocation(!geolocation))
+    if (!animationEnd) {
+      dispatch(activeAnimation(true))
+    } else {
+      dispatch(getWeatherDataByCoordinates(host));
+    }
 
+
+  };
   /*
    *
    */
+  const handlerAnimationEnd = (e) => {
+    console.log("CityField component - handlerAnimationEnd");
+    dispatch(setAnimationEnd(true));
+    if (geolocation) {
+      dispatch(getWeatherDataByCoordinates(host));
+    } else if (cityField) {
+      dispatch(selectCityName(cityField));
+      let options = {
+        getForecastData: true,
+      };
+      dispatch(fetchWeatherData(host, cityField, options));
+    } else {
+      /**
+       * not found
+       */
+    }
+
+  };
+  /**
+   * 
+   * @param {*} evt 
+   */
+  const onPressEnterKey = (evt) => {
+    console.log("CityField component - onPressEnterKey");
+    evt.preventDefault();
+    if (!animationEnd) {
+      dispatch(activeAnimation(true))
+    } else {
+      dispatch(selectCityName(cityField));
+      dispatch(setCoordinates({ latitude: null, longitude: null }));
+      let options = {
+        getForecastData: true,
+      };
+      dispatch(fetchWeatherData(host, cityField, options));
+    }
+
+  };
+  /*
+   *
+   * @param {*} evt
+   */
+  const handlerCityField = (evt) => {
+    console.log("CityField component - handlerCityField");
+    if (geolocation) {
+      dispatch(enableGeolocation(false))
+      dispatch(setCoordinates({ latitude: null, longitude: null }));
+    }
+    setCityField(evt.target.value);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setCityField(city)
+    }
+  }, [city])
+  /*
+   *
+   *
   const handlerSwitch = () => {
     console.log("CityField component - handlerSwitch");
     dispatch(enableGeolocation(!geolocation));
@@ -47,7 +119,7 @@ const CityField = memo(({ placeholder }) => {
   };
   /*
    *
-   */
+   *
   const handlerAnimationEnd = () => {
     console.log("CityField component - handlerAnimationEnd");
     dispatch(setAnimationEnd(true));
@@ -60,12 +132,12 @@ const CityField = memo(({ placeholder }) => {
         getForecastData: true,
       };
       dispatch(fetchWeatherData(host, cityField, options));
-    }*/
+    }
   };
   /*
    *
    * @param {*} evt
-   */
+   *
   const onPressEnterKey = (evt) => {
     console.log("CityField component - onPressEnterKey");
     evt.preventDefault();
@@ -83,7 +155,7 @@ const CityField = memo(({ placeholder }) => {
   /**
    *
    * @param {*} evt
-   */
+   *
   const handlerCityField = (evt) => {
     console.log("CityField component - handlerCityField");
     if (geolocation) {
@@ -94,7 +166,7 @@ const CityField = memo(({ placeholder }) => {
   };
   /*
    *
-   */
+   *
   useEffect(() => {
     console.log("CityField component - useEffect by city field");
     setCityField(city);
@@ -108,7 +180,7 @@ const CityField = memo(({ placeholder }) => {
   }, [city]);
   /**
    *
-   */
+   *
   useEffect(() => {
     console.log(
       "CityField component - useEffect by animationEnd",
@@ -118,28 +190,30 @@ const CityField = memo(({ placeholder }) => {
       if (geolocation) {
         dispatch(getWeatherDataByCoordinates(host));
       }
-      /* if (city) {
+       if (city) {
         console.log("eccolo");
         dispatch(selectCityName(cityField));
         let options = {
           getForecastData: true,
         };
         dispatch(fetchWeatherData(host, cityField, options));
-      }*/
+      }
     }
-  }, [animationEnd, geolocation]);
+  }, [animationEnd, geolocation]);*/
 
   /*
    *
    */
   return (
     <div
-      onAnimationEnd={handlerAnimationEnd}
+
       className={enableAnimation ? "cityFieldContainer2" : "cityFieldContainer"}
-      /*style={enableAnimation ? { animationName: "example" } : {} null}*/
+      onAnimationEnd={handlerAnimationEnd}
+    /*style={enableAnimation ? { animationName: "example" } : {} null}*/
     >
       <form onSubmit={onPressEnterKey} style={{ width: "100%" }}>
         <Input
+
           onChange={handlerCityField}
           value={cityField}
           className="cityField"
@@ -148,13 +222,13 @@ const CityField = memo(({ placeholder }) => {
           prefix={<SearchOutlined />}
         />
       </form>
-      <Popover content={<span>Usa la tua posizione corrente</span>}>
-        <Switch
-          style={{ marginLeft: 10 }}
-          onChange={handlerSwitch}
-          checked={geolocation}
-        />
-      </Popover>
+
+      <input className="myinput" type='checkbox'
+        title="Usa la tua posizione corrente"
+        style={{ marginLeft: 10 }}
+        onChange={handlerSwitch}
+        checked={geolocation}
+      />
     </div>
   );
 });
